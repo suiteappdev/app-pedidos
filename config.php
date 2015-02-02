c882ea9ffb58fb5df111d6a01f039870d54d5b4e
andrew castro <listerine1989@gmail.com>
2015-02-02 16:55:43 -0500

    Titulo de la pagina
    se agregó la variable title en el controlador index

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+define('URL', 'http://localhost/');
+define('LIBS', 'libs/');
+define('UPLOADS_DIR', 'public/uploads/');
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'suiteapp_johnvery');
+define('DB_USER', 'root');
+define('DB_PASS', "");
+
+define('APP_NAME','Ecommerce administracion');
+define('MIEMPRESA', 1);
+define('APP_VERSION','1.0');
+
+define('HASH_GENERAL_KEY', 'MixitUp200');
+define('HASH_PASSWORD_KEY', 'M3gaS3nd20014R3leAs3');
+define('HASH_ACTIVATE_KEY', 'MegaSend@CtIvAt3K3y');
